cb507a8f17cf71c626a04bda31e0c84ba5e79158
aya0224 <a.tsuchiya.0224@gmail.com>
2025-07-17 08:35:31 +0900

    訂正

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -2,25 +2,41 @@ package baseball;
 
 public class BaseBallTeam {
     
-   public String name;
-   public int winScore;
-   public int loseScore;
-   public int drawScore;
+   private String name;
+   private int winScore;
+   private int loseScore;
+   private int drawScore;
+   
+   public String getName() {
+       return this.name;
+   }
+   
+   public int getWinScore() {
+       return this.winScore;
+   }
+   
+   public int getLoseScore() {
+       return this.loseScore;
+   }
+   
+   public int getDrawScore() {
+       return this.drawScore;
+   }
    
  public BaseBallTeam() {
        
    }
    
-public BaseBallTeam(String name,int winScore,int loseScore,int drawscore) {
-    this.name = name;
-    this.winScore =winScore;
-    this.loseScore =loseScore;
-    this.drawScore = drawScore;
+public BaseBallTeam(String name,int winScore,int loseScore,int drawScore) {
+    this.setName(name);
+    this.setWinScore(winScore);
+    this.setLoseScore(loseScore);
+    this.setDrawScore(drawScore);
 }
 
 
 public double getRate() {
-       double rate = (double)(this.winScore) / (double)(this.winScore + this.loseScore);
+       double rate = (double)(this.getWinScore()) / (double)(this.getWinScore() + this.getLoseScore());
        return rate;
    }  
 
@@ -29,9 +45,25 @@ public double getRate() {
 
 public void report() {
     
-    System.out.println(name + "の２０２２年の成績は" + winScore +"勝" + loseScore +"敗" + drawScore + "勝率は" + getRate() );
+    System.out.println(getName() + "の２０２２年の成績は" + getWinScore() +"勝" + getLoseScore() +"敗" + getDrawScore() + "勝率は" + getRate() );
     
 
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public void setDrawScore(int drawScore) {
+    this.drawScore = drawScore;
+}
+
+public void setWinScore(int winScore) {
+    this.winScore = winScore;
+}
+
+public void setLoseScore(int loseScore) {
+    this.loseScore = loseScore;
 }
 
 
--- src/baseball/Review03.java
+++ src/baseball/Review03.java
@@ -5,49 +5,49 @@ public class Review03 {
     public static void main(String[] args) {
         BaseBallTeam team01 = new BaseBallTeam();
         team01.setName("東京ヤクルトスワローズ");
-        team01.winScore = 80;
-        team01.loseScore = 59;
-        team01.drawScore = 4;
+        team01.setWinScore(80);
+        team01.setLoseScore(59);
+        team01.setDrawScore(4);
         
         team01.report();
         
         BaseBallTeam team02 = new BaseBallTeam();
         team02.setName("横浜DeNAベイスターズ");
-        team02.winScore = 73;
-        team02.loseScore = 68;
-        team02.drawScore = 2;
+        team02.setWinScore(73);
+        team02.setLoseScore(68);
+        team02.setDrawScore(2);
         
         team02.report();
         
         BaseBallTeam team03 = new BaseBallTeam();
         team03.setName("阪神タイガース");
-        team03.winScore = 68;
-        team03.loseScore = 71;
-        team03.drawScore = 2;
+        team03.setWinScore(68);
+        team03.setLoseScore(71);
+        team03.setDrawScore(2);
         
         team03.report();
         
         BaseBallTeam team04 = new BaseBallTeam();
         team04.setName("読売ジャイアンツ");
-        team04.winScore = 68;
-        team04.loseScore = 72;
-        team04.drawScore = 3;
+        team04.setWinScore(68);
+        team04.setLoseScore(72);
+        team04.setDrawScore(3);
         
         team04.report();
         
         BaseBallTeam team05 = new BaseBallTeam();
         team05.setName("広島東洋カープ");
-        team05.winScore = 66;
-        team05.loseScore = 74;
-        team05.drawScore = 3;
+        team05.setWinScore(66);
+        team05.setLoseScore(74);
+        team05.setDrawScore(3);
         
         team05.report();
         
         BaseBallTeam team06 = new BaseBallTeam();
         team06.setName("中日ドラゴンズ");
-        team06.winScore = 66;
-        team06.loseScore = 75;
-        team06.drawScore = 2;
+        team06.setWinScore(66);
+        team06.setLoseScore(75);
+        team06.setDrawScore(2);
         
         team06.report();
 
